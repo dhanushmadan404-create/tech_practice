04b3723194305d61bd755053f52f9100f75085a4
Dhanush Kumar M <dhanushmadan404@gmail.com>
2025-10-22 16:28:47 +0530

    task2,3_push

--- task2.py
+++ task2.py
@@ -3,3 +3,18 @@ shapes = ("circle", "square", "triangle")
 # act1=1
 act=colors+shapes
 print(act)
+print("")
+# act1=2
+print(colors*3)
+print("")
+# act1=3
+act=act+("Diamond",)
+print(act)
+print("")
+# act1=4
+if "square" in act:
+    print(True)
+else:
+    print(False)
+print("")
+
