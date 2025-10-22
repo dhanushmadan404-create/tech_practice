57eb2741de5ab64cd6fbdc58e1af186dd93a0070
Dhanush Kumar M <dhanushmadan404@gmail.com>
2025-10-22 16:50:48 +0530

    task3 and 4 finished

---
+++ task3.py
@@ -0,0 +1,9 @@
+# act3_1
+marks = (78, 85, 69, 90, 85)
+print(max(marks))
+print(min(marks))
+print(marks.count(85))
+# average
+b=len(marks)
+sum=sum(marks)
+print(sum/b)
---
+++ task4.py
@@ -0,0 +1,11 @@
+monthly_rainfall = (120, 150, 120, 180, 120, 90, 110, 130, 100, 140, 120, 160)
+
+# 1
+print(sum(monthly_rainfall))
+# 2 average
+print(int(sum(monthly_rainfall)/len(monthly_rainfall)))
+# 3
+print(monthly_rainfall.count(120))
+# 4
+print(max(monthly_rainfall))
+print(min(monthly_rainfall))
